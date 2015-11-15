030f9229cd18f5051f0f39455b373114fed9040c
richfergus <richfergus@yahoo.com>
2015-11-15 07:22:00 -0500

    updated

--- js/app.js
+++ js/app.js
@@ -1,19 +1,8 @@
+
 var myApp = angular.module('myApp',
   ['ngRoute', 'firebase'])
   .constant('FIREBASE_URL', 'https://nsf-class-selector.firebaseio.com/');
 
-
-myApp.run(['$rootScope', '$location',
-   function($rootScope, $location) {
-      $rootScope.$on('$routeChangeError',
-          function(event, next, previous, error){
-              if (error == 'AUTH_REQUIRED') {
-                $rootScope.message = 'Sorry, you must log in to accsess that page';
-                $location.path('/logon');
-              }
-          });
-  
-}]);
 
 myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
@@ -46,3 +35,32 @@ myApp.config(['$routeProvider', function($routeProvider) {
       redirectTo: '/login'
     });
 }]);
+
+myApp.run(['$rootScope', '$location',
+   function($rootScope, $location) {
+      $rootScope.$on('$routeChangeError',
+          function(event, next, previous, error){
+              if (error == 'AUTH_REQUIRED') {
+                $rootScope.message = 'Sorry, you must log in to accsess that page';
+                $location.path('/logon');
+              }
+          });
+   
+}]);
+
+angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
+
+angular.module('ui.bootstrap.demo').controller('AlertDemoCtrl', function ($scope) {
+  $scope.alerts = [
+    { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+    { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+  ];
+
+  $scope.addAlert = function() {
+    $scope.alerts.push({msg: 'Another alert!'});
+  };
+
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
+}); 
