a0f59cc76e6268ff065435cc75367908a01a94ac
richfergus <richfergus@yahoo.com>
2015-11-15 20:56:41 -0500

    big update

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 var myApp = angular.module('myApp',
-  ['ngRoute', 'firebase', 'ngAnimate', 'ui.bootstrap'])
-  .constant('FIREBASE_URL', 'https://nsf-class-selector.firebaseio.com/');
+  ['ngRoute', 'firebase', 'ngAnimate', 'ngResource', 'datatables', 'ui.bootstrap'])
+  .constant('FIREBASE_URL', 'https://nsf-class-selector.firebaseio.com/').controller('AngularWayChangeDataCtrl', AngularWayChangeDataCtrl);
 
 myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
@@ -35,9 +35,51 @@ myApp.config(['$routeProvider', function($routeProvider) {
     });
 }]);
 
-angular.module('myApp').controller('CollapseDemoCtrl', function ($scope) {
-  $scope.isCollapsed = false;
-});
+
+function AngularWayChangeDataCtrl($resource, DTOptionsBuilder, DTColumnDefBuilder) {
+    var vm = this;
+    vm.courses = $resource('hks.json').query();
+    vm.dtOptions = DTOptionsBuilder.newOptions().withPaginationType('full_numbers');
+    vm.dtColumnDefs = [
+        DTColumnDefBuilder.newColumnDef(0),
+        DTColumnDefBuilder.newColumnDef(1),
+        DTColumnDefBuilder.newColumnDef(2),
+        DTColumnDefBuilder.newColumnDef(3).notSortable()
+    ];
+    vm.courses2add = _buildCourse2Add(1);
+    vm.addCourse = addCourse;
+    vm.modifyCourse = modifyCourse;
+    vm.removeCourse = removeCourse;
+
+    function _buildCourse2Add(Number) {
+        return {
+            Number: 'Number',
+            Days: 'Days',
+            Semester: 'Semester'
+        };
+    }
+    function addCourse() {
+        vm.courses.push(angular.copy(vm.courses2add));
+        vm.courses2add = _buildCourse2Add(vm.courses2add.Number + 1);
+    }
+    function modifyCourse(index) {
+        vm.courses.splice(index, 1, angular.copy(vm.courses2add));
+        vm.courses2add = _buildCourse2Add(vm.courses2add.Number + 1);
+    }
+    function removeCourse(index) {
+        vm.courses.splice(index, 1);
+    }
+}
+
+
+
+
+
+    // $resource('hks.json').query().$promise.then(function(courses) {
+    //     vm.courses = courses;
+    // });
+
+
 angular.module('myApp').controller('DropdownCtrl', function ($scope, $log) {
 
 });
