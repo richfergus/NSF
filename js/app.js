a5fb8f89f6b0a922c17566dd0aee63798f0b28ef
richfergus <richfergus@yahoo.com>
2015-11-18 22:36:05 -0500

    big update

--- js/app.js
+++ js/app.js
@@ -28,7 +28,13 @@ angular.module('myApp').controller('ButtonsCtrl', function ($scope) {
   });
 });
 
-
+angular.module('myApp').controller('basicsCtrl', ['$scope', function ($scope) {
+    scope.rowCollection = [
+        {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
+        {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
+        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+    ];
+}]);
 
 myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
