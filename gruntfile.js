6d1f2b96d00031f2d754a7b6384164f6a8390c88
richfergus <richfergus@yahoo.com>
2015-11-30 20:03:17 -0500

    after grunt

--- gruntfile.js
+++ gruntfile.js
@@ -2,13 +2,34 @@ module.exports = function(grunt){
 	grunt.initConfig({
 
 		concat: {
+			options: {
+				separator:'\n\n //------------------------>\n',
+				banner:'\n\n //--------------->\n\n'
+			},
 			dist:{
 				src: 'builds/*.js',
 				dest:'js/script.js'
 
 			}
+		},
+
+		watch: {
+			options:{
+				spawn:false
+			},
+			scripts:{
+			files:['views/**/*.html',
+			'js/contollers/*.js',
+			'css/*.css'
+			],
+			tasks:['concat']
+		}	
+
 		}
 	});
 grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-watch');
+
+grunt.registerTask('default', ['concat', 'watch']);
 
 };
